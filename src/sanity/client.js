8d9424d232ef250e0dad88251ddf877b3968191b
Shasa <shasa.thuo@gmail.com>
2022-07-10 21:16:21 +0300

    connect sanity to client

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -1,1 +1,14 @@
 // sanity config documents go in here
+import sanityClient from '@sanity/client';
+import { imageUrlBuilder } from '@sanity/image-url';
+
+export const client = sanityClient({
+    projectId: process.env.REACT_APP_SHAREME_SOCIAL_SANITY_ID,
+    dataset:'production',
+    apiVersion:'2022-07-10',
+    useCdn: true,
+    token: process.env.REACT_APP_SHAREME_TOKEN
+})
+
+const builder = imageUrlBuilder((client));
+export const urlFor = (source) => builder.image(source)
